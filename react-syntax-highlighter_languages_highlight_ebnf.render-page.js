b856eb54b0bed8576705f4ef457a1ceaee9ad069
junior291492659 <jarin.li@porsche.digital>
2023-06-08 14:18:06 +0800

    Updates

---
+++ react-syntax-highlighter_languages_highlight_ebnf.render-page.js
@@ -0,0 +1,54 @@
+exports.ids = ["react-syntax-highlighter_languages_highlight_ebnf"];
+exports.modules = {
+
+/***/ "../highlight.js/lib/languages/ebnf.js":
+/*!*********************************************!*\
+  !*** ../highlight.js/lib/languages/ebnf.js ***!
+  \*********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = function(hljs) {
+    var commentMode = hljs.COMMENT(/\(\*/, /\*\)/);
+
+    var nonTerminalMode = {
+        className: "attribute",
+        begin: /^[ ]*[a-zA-Z][a-zA-Z-_]*([\s-_]+[a-zA-Z][a-zA-Z]*)*/
+    };
+
+    var specialSequenceMode = {
+        className: "meta",
+        begin: /\?.*\?/
+    };
+
+    var ruleBodyMode = {
+        begin: /=/, end: /[.;]/,
+        contains: [
+            commentMode,
+            specialSequenceMode,
+            {
+              // terminals
+              className: 'string',
+              variants: [
+                hljs.APOS_STRING_MODE,
+                hljs.QUOTE_STRING_MODE,
+                {begin: '`', end: '`'},
+              ]
+            },
+        ]
+    };
+
+    return {
+        illegal: /\S/,
+        contains: [
+            commentMode,
+            nonTerminalMode,
+            ruleBodyMode
+        ]
+    };
+};
+
+/***/ })
+
+};;
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_ebnf.render-page.js.map
---
+++ react-syntax-highlighter_languages_highlight_ini.render-page.js
@@ -0,0 +1,91 @@
+exports.ids = ["react-syntax-highlighter_languages_highlight_ini"];
+exports.modules = {
+
+/***/ "../highlight.js/lib/languages/ini.js":
+/*!********************************************!*\
+  !*** ../highlight.js/lib/languages/ini.js ***!
+  \********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = function(hljs) {
+  var NUMBERS = {
+    className: 'number',
+    relevance: 0,
+    variants: [
+      { begin: /([\+\-]+)?[\d]+_[\d_]+/ },
+      { begin: hljs.NUMBER_RE }
+    ]
+  };
+  var COMMENTS = hljs.COMMENT();
+  COMMENTS.variants = [
+    {begin: /;/, end: /$/},
+    {begin: /#/, end: /$/},
+  ];
+  var VARIABLES = {
+    className: 'variable',
+    variants: [
+      { begin: /\$[\w\d"][\w\d_]*/ },
+      { begin: /\$\{(.*?)}/ }
+    ]
+  };
+  var LITERALS = {
+    className: 'literal',
+    begin: /\bon|off|true|false|yes|no\b/
+  };
+  var STRINGS = {
+    className: "string",
+    contains: [hljs.BACKSLASH_ESCAPE],
+    variants: [
+      { begin: "'''", end: "'''", relevance: 10 },
+      { begin: '"""', end: '"""', relevance: 10 },
+      { begin: '"', end: '"' },
+      { begin: "'", end: "'" }
+    ]
+  };
+  var ARRAY = {
+    begin: /\[/, end: /\]/,
+    contains: [
+      COMMENTS,
+      LITERALS,
+      VARIABLES,
+      STRINGS,
+      NUMBERS,
+      'self'
+    ],
+    relevance:0
+  };
+
+  return {
+    aliases: ['toml'],
+    case_insensitive: true,
+    illegal: /\S/,
+    contains: [
+      COMMENTS,
+      {
+        className: 'section',
+        begin: /\[+/, end: /\]+/
+      },
+      {
+        begin: /^[a-z0-9\[\]_\.-]+(?=\s*=\s*)/,
+        className: 'attr',
+        starts: {
+          end: /$/,
+          contains: [
+            COMMENTS,
+            ARRAY,
+            LITERALS,
+            VARIABLES,
+            STRINGS,
+            NUMBERS
+          ]
+        }
+      }
+    ]
+  };
+};
+
+/***/ })
+
+};;
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_ini.render-page.js.map
